bb8bf0bf8c8bb81ac55b57e1a156adbc2e449839
darshan11416 <darshanchaudhari5504@gmail.com>
2024-08-08 20:50:30 +0530

    Updated

---
+++ src/main/java/com/example/smartlogistics/SmartLogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartlogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLogisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartLogisticsApplication.class, args);
+    }
+
+}
